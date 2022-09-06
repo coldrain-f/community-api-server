4cecffc2544ce2f42391eb4382a31fd2eb98d31d
coldrain-f <intmain__@naver.com>
2022-09-06 11:39:16 +0900

    feat: Board 엔티티 설계

--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,1 +1,55 @@
-export class Board {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    comment: '해당 컬럼은 게시글의 제목을 나타냅니다.',
+  })
+  title: string;
+
+  @Column({
+    type: 'varchar',
+    length: 200,
+    comment: '해당 컬럼은 게시글의 본문을 나타냅니다.',
+  })
+  content: string;
+
+  @Column({
+    type: 'varchar',
+    comment: '해당 컬럼은 게시글의 비밀번호를 나타냅니다.',
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    comment: '해당 컬럼은 게시글 업로드 시점의 날씨를 나타냅니다.',
+  })
+  weather: string;
+
+  @CreateDateColumn({
+    comment: '해당 컬럼은 게시글 등록일을 나타냅니다.',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    comment: '해당 컬럼은 게시글 수정일을 나타냅니다.',
+  })
+  updatedAt: Date;
+
+  @Column({
+    default: false,
+    comment: '해당 컬럼은 게시글의 삭제 여부를 나타냅니다.',
+  })
+  isDeleted: boolean;
+}
