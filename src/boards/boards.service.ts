b6b5766a713c68dff67427b8de89c1b0b3cfe052
coldrain-f <intmain__@naver.com>
2022-09-06 13:05:50 +0900

    feat: 게시글 수정 API 구현
    - 요청 body의 password와 DB의 password와 비교해서 같으면 수정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -10,6 +10,7 @@ import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
 import { Repository } from 'typeorm';
+import { raw } from 'express';
 
 @Injectable()
 export class BoardsService {
@@ -38,22 +39,31 @@ export class BoardsService {
     return `This action returns all boards`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} board`;
-  }
-
-  update(id: number, updateBoardDto: UpdateBoardDto) {
-    return `This action updates a #${id} board`;
-  }
-
-  async remove(id: number, password: string) {
+  async findOne(id: number) {
     const board = await this.boardRepository.findOne({
       where: { id, isDeleted: false },
     });
-
     if (!board) {
       throw new NotFoundException('게시글을 찾을 수 없습니다.');
     }
+    return board;
+  }
+
+  async update(id: number, updateBoardDto: UpdateBoardDto) {
+    const { title, content, password } = updateBoardDto;
+    const board = await this.findOne(id);
+
+    if (!(await bcrypt.compare(password, board.password))) {
+      throw new ForbiddenException('비밀번호가 일치하지 않습니다.');
+    }
+
+    board.title = title;
+    board.content = content;
+    await this.boardRepository.save(board);
+  }
+
+  async remove(id: number, password: string) {
+    const board = await this.findOne(id);
 
     if (!(await bcrypt.compare(password, board.password))) {
       throw new ForbiddenException('비밀번호가 일치하지 않습니다.');
--- src/boards/dto/update-board.dto.ts
+++ src/boards/dto/update-board.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBoardDto } from './create-board.dto';
+import { PickType } from '@nestjs/swagger';
+import { Board } from '../entities/board.entity';
 
-export class UpdateBoardDto extends PartialType(CreateBoardDto) {}
+export class UpdateBoardDto extends PickType(Board, [
+  'title',
+  'content',
+  'password',
+]) {}
