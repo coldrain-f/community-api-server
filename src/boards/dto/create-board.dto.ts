c33d9b753cc0c6778c69e72567aff292a20642a0
coldrain-f <intmain__@naver.com>
2022-09-06 17:17:40 +0900

    feat: 게시글 등록, 수정 DTO Swagger 설정 추가
    - example
    - description
    - required

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,16 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 
 export class CreateBoardDto {
+  @ApiProperty({
+    example: '백견이불여일타 JSP & Servlet',
+    description: '게시글의 제목입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Length(1, 20)
   title: string;
 
+  @ApiProperty({
+    example: '페이지네이션과 필터링의 차이는...',
+    description: '게시글의 본문입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Length(1, 200)
   content: string;
 
+  @ApiProperty({
+    example: 'ppppp1',
+    description: '게시글의 비밀번호입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^(?=.*?[0-9]).{6,}$/, {
--- src/boards/dto/update-board.dto.ts
+++ src/boards/dto/update-board.dto.ts
@@ -1,16 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class UpdateBoardDto {
+  @ApiProperty({
+    example: '백견이불여일타 JSP & Servlet',
+    description: '게시글의 제목입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Length(1, 20)
   title: string;
 
+  @ApiProperty({
+    example: '페이지네이션과 필터링의 차이는...',
+    description: '게시글의 본문입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Length(1, 200)
   content: string;
 
+  @ApiProperty({
+    example: 'ppppp1',
+    description: '게시글의 비밀번호입니다.',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
