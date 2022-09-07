1b966d5f2f805c4492e7c8458c5fd4542278434d
coldrain-f <intmain__@naver.com>
2022-09-07 12:32:26 +0900

    test: 게시글 API e2e 테스트 완료
    - 게시글 등록
    - 게시글 목록 조회
    - 게시글 수정
    - 게시글 삭제

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -20,5 +20,49 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  describe('게시글 등록 API', () => {
+    it('/boards (POST) return 201', async () => {
+      const res = await request(app.getHttpServer()).post('/boards').send({
+        title: 'title',
+        content: 'content',
+        password: 'password0',
+      });
+      expect(res.statusCode).toBe(HttpStatus.CREATED);
+    });
+  });
+
+  describe('게시글 목록 조회 API', () => {
+    it('/boards (GET) return 400', () => {
+      return request(app.getHttpServer())
+        .get('/boards')
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('/boards?page=1 (GET) return 200', async () => {
+      const res = await request(app.getHttpServer()).get('/boards?page=1');
+      expect(res.statusCode).toBe(HttpStatus.OK);
+    });
+  });
+
+  describe('게시글 수정 API', () => {
+    it('/boards/:id (PATCH) return 200', async () => {
+      const res = await request(app.getHttpServer()).patch('/boards/13').send({
+        title: 'title modify',
+        content: 'content modify',
+        password: 'password01',
+      });
+      expect(res.statusCode).toBe(HttpStatus.OK);
+    });
+  });
+
+  describe('게시글 삭제 API', () => {
+    it('/boards/:id (DELETE) return 200', async () => {
+      const res = await request(app.getHttpServer()).delete('/boards/12').send({
+        password: 'password0',
+      });
+      expect(res.statusCode).toBe(HttpStatus.OK);
+    });
   });
 });
